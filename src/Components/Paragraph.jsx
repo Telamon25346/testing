9ca248f9c89c8eac02556ec0ed726e99ef3eeee7
telamon25346 <telamon25346@gmail.com>
2023-04-30 01:17:19 -0500

    changed bullet points from white to blue

--- src/Components/Paragraph.jsx
+++ src/Components/Paragraph.jsx
@@ -5,7 +5,7 @@ function Paragraph() {
   return (
     <>
     <div>
-      <h1>Fun facts about React</h1>
+      <h1 className='funfacts'>Fun facts about React</h1>
       <ul>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>

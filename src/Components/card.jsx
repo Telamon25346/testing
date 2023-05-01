8c23ff58729662d89d1e6d0e149f87b249996a3f
telamon25346 <telamon25346@gmail.com>
2023-05-01 18:34:12 -0500

    updated site

---
+++ src/Components/card.jsx
@@ -0,0 +1,23 @@
+import './card.css'
+import ManImage from '../assets/man.png'
+import StarImage from '../assets/star.png'
+
+function CardBoy () {
+    return (
+        <>
+        <div className='card'>
+            <img src={ManImage}></img>
+            <div className='cardstats'>
+                <img src={StarImage} className='starimage'></img>
+                <span>5.0</span>
+                <span className='rating'>(6) •</span>
+                <span className='USA'>USA</span>
+            </div>
+            <p className='lifelessons'> Life Lessons with Nigga Nigagathan</p>
+            <p> <span className='bold'> From $126 </span> / person</p>
+        </div>
+        </>
+    )
+}
+
+export default CardBoy; 
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import NavBar from './Components/navbar.jsx'
 import HeroPage from './Components/heropage.jsx'
 import './index.css'
+import CardBoy from './Components/card.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NavBar />
     <HeroPage />
+    <CardBoy />
   </React.StrictMode>,
 )
